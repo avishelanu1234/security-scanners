7f88f0807440b05bbf809296f219629ef3983415
avishelanu1234 <avi_shelanu@walla.com>
2026-04-21 18:03:06 +0300

    Refactored SQL injection detection logic to minimize false positives.

--- sast_runner.py
+++ sast_runner.py
@@ -43,10 +43,13 @@ def detect_vulnerabilities(input_string):
         r'^[\w_.+-]+@[\w-]+\.[a-zA-Z]{2,}$',  # Valid email format
         r'^[\w_]+$',  # Alphanumeric usernames
         r'^[\d]+$',  # Numeric input
-        r'^[\w_]+@\w+\.\w{2,3}$',  # Shortened email format
+        r'^[\w_]+@[\w]+\.[\w]{2,3}$',  # Shortened email format
         r'^[\d]{1,5}$',  # Numeric input within 1 to 5 digits
-        r'^[\w_]+\s*\w*$'  # Two-word usernames
-    ]  # Example patterns
+        r'^[\w_]+\s*\w*$',  # Two-word usernames
+        r'^[\-a-zA-Z]+$',  # Allow hyphenated words
+        r'^[\w\s]+$',  # Alphanumeric with spaces
+        r'^[\w\s]+[\.\,\'\"\-]+[\w\s]+$'  # Allows punctuation between words
+    ]
     
     # Check against the whitelisted patterns
     if any(re.match(pattern, input_string) for pattern in acceptable_patterns):
